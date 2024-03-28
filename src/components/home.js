d9bfc3e952270605e8ff8b26b2385b45e87eb438
MATHEUS SILVA DA CRUZ <matheus_silva-cruz@estudante.sesisenai.org.br>
2024-03-28 09:12:43 -0300

    Adiciona Portfolios

--- src/components/home.js
+++ src/components/home.js
@@ -19,6 +19,11 @@ export const Home = () =>{
             em React, Node.js e Java. Meu objetivo é me tornar um desenvolvedor FullStack. E conseguir um estágio em uma empresa de tecnologia<br></br>
           </p>
         </div>
+        <div className="portfolios">
+          <h3>Portfólios anteriores:</h3>
+          <a href="https://www.canva.com/design/DAE9G9WvBpQ/i3tcIBCqk2oaM_BLxiuNXQ/edit?utm_content=DAE9G9WvBpQ&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"><button className="portfolio">Portfólio 2022</button></a>    
+          <a href="https://www.canva.com/design/DAFxtkB93nA/iUg8nZJwq4JvNgzCZOQsWw/edit?utm_content=DAFxtkB93nA&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"><button className="portfolio">Porfólio 2023</button></a>
+        </div>
       </div>
     </div>
   );
