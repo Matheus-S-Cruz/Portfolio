71ff6017c7f5e6b118245dec0370a3dbdae0759c
Matheus_S-Cruz <matheus_silva-cruz@estudante.sesisenai.org.br>
2024-04-03 20:25:28 -0300

    Altera home

--- src/components/home.js
+++ src/components/home.js
@@ -15,7 +15,7 @@ export const Home = () =>{
         <div className="container">
           <div id="sobre" className="sobre">
             <h3>Sobre Mim:</h3>
-            <p>Estudante de Desenvolvimento de Sistemas no SENAI, no terceiro ano do Ensino Médio, com previsão de formatura em 2024.
+            <p>Estudante de Desenvolvimento de Sistemas no SENAI, no terceiro ano do Ensino Médio, com previsão de conclusão em dezembro de 2024.
               Me interessei muito pela programação em 2022, quando fiz um curso FullStack do programa FuturoDev.
               Atualmente, estou explorando mais na área de Desenvolvimento de Sistemas, e pretendo me aprofundar mais
               em React, Node.js e Java. Meu objetivo é me tornar um desenvolvedor FullStack e conseguir um estágio em uma empresa de tecnologia.
