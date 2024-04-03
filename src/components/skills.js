18215b887214e9d71aac77c5ffddc9cdc67c788d
Matheus Cruz <matheus_silva-cruz@estudante.sesisenai.org.br>
2024-04-03 11:17:04 -0300

    Adiciona Skills

--- src/components/skills.js
+++ src/components/skills.js
@@ -2,7 +2,13 @@ export const Skills = () =>{
 
     return (
       <div className="Skills">
-        
+      <h2>Soft Skills</h2>
+      <div className="skills">
+            <p><span>Proatividade</span></p>
+            <p><span>Cooperação</span></p>
+            <p><span>Rápida Aprendizagem</span></p>
+            <p><span>Resolução de Problemas</span></p>
+      </div>
       </div>
     );
   }
