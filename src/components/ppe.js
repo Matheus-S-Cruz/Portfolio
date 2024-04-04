7cca00762e4f33d0c2aeab4895df285e38b700b0
Matheus Cruz <matheus_silva-cruz@estudante.sesisenai.org.br>
2024-04-04 11:31:56 -0300

    Ataliza PPE para navegar com NavBar

--- src/components/ppe.js
+++ src/components/ppe.js
@@ -1,7 +1,7 @@
 export const PPE = () =>{
 
     return(
-        <div className="PPE">
+        <div className="PPE" id="PPE">
              <h3>PPE</h3>
              <p>O PPE ainda está em construção, espere mais um pouco.</p>
              <a href=""><button className="ppe">PPE</button></a> 
