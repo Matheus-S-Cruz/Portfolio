c84fda36702829f2a67dd1b0156afd90eb6244ed
Matheus_S-Cruz <matheus_silva-cruz@estudante.sesisenai.org.br>
2024-08-16 16:15:34 -0300

    Finaliza atividades do técnico

--- src/components/tecnico.js
+++ src/components/tecnico.js
@@ -5,6 +5,16 @@ import calculadora from "../images/calculadora.png";
 import livro from "../images/livro.png";
 import diagrama from "../images/diagrama.png";
 import implantacao from "../images/implantacao.png";
+import sa from "../images/sa.png";
+import regras from "../images/regras.png";
+import kanban from "../images/kanban.png";
+import logico from "../images/logico.png";
+import medico from "../images/medico.png";
+import normalização from "../images/normalização.png";
+import api from "../images/api.png";
+import apigithub from "../images/apigithub.png";
+import biblio from "../images/biblio.png";
+import joomla from "../images/joomla.png";
 
 export const Tecnico = () => {
   const [selectedCategory, setSelectedCategory] = useState("desenvolvimento");
@@ -29,6 +39,33 @@ export const Tecnico = () => {
       trimestre: "primeiro",
       link: "https://docs.google.com/document/d/1kNYXvBqI5UG5ESheDR-8i--RrXXFbfvZ6WpvuiZ96l8/edit?usp=sharing",
     },
+    {
+      titulo: "API Github",
+      descricao:
+        "Nessa atividade fizemos um projeto que utiliza uma API do github para buscar algum usuário da plataforma. ",
+      mais: "Clique para ver mais sobre a atividade",
+      imgUrl: apigithub,
+      trimestre: "primeiro",
+      link: "https://docs.google.com/document/d/1fkAOeQYp9Z8HD26QfHf2hNH3QQw0L0teq8TRDYZQ_nY/edit?usp=sharing",
+    },
+    {
+      titulo: "Trabalho API",
+      descricao:
+        "Nessa atividade fizemos um trabalho sobre as APIs, usando canva. ",
+      mais: "Clique para ver mais sobre a atividade",
+      imgUrl: api,
+      trimestre: "primeiro",
+      link: "https://docs.google.com/document/d/1Gmwithv0Z1mweWYEW7oErSUrx7-A3l5sgDctOSB3dlU/edit?usp=sharing",
+    },
+    {
+      titulo: "Projeto Biblioteca",
+      descricao:
+        "Nesse projeto nós fizemos um sistema de biblioteca com SpringBoot, onde aprendi sobre o uso de métodos HTTP, REST, e o uso de um banco de dados. ",
+      mais: "Clique para ver mais sobre a atividade",
+      imgUrl: biblio,
+      trimestre: "segundo",
+      link: "https://drive.google.com/drive/folders/1Ve9QU9DxRJiJUWMmGCIGWYnIAmOpoqVr?usp=sharing",
+    },
   ];
 
   const implantação = [
@@ -41,6 +78,15 @@ export const Tecnico = () => {
       trimestre: "primeiro",
       link: "https://docs.google.com/spreadsheets/d/1s7CjzyajRil_DIJ7Vb_pDFbP7tmqHyI91xOadTypNW8/edit?usp=sharing",
     },
+    {
+      titulo: "Cenario 1 Atividade 1",
+      descricao:
+        "Nessa atividade nós fizemos um documento com o objetivo de propor um modelo de instalação para a empresa Veículos Brasil, este sistema seria gerenciado pela Joomla. ",
+      mais: "Clique para ver mais sobre a atividade",
+      imgUrl: joomla,
+      trimestre: "primeiro",
+      link: "https://docs.google.com/document/d/1aAl0wvX8bZFa3VQgdwSJ4vRkb1k41HXRXE888Opl7d4/edit?usp=sharing",
+    },
   ];
 
   const modelagem = [
@@ -52,6 +98,63 @@ export const Tecnico = () => {
       imgUrl: diagrama,
       trimestre: "primeiro",
       link: "https://docs.google.com/document/d/1tpwqw4gZqRbBlWCwBww9a8dNlh7ycf5K-P-KOqtE_D0/edit?usp=sharing",
+    },
+    {
+      titulo: "Wireframe Alta Fidelidade",
+      descricao:
+        "Nessa atividade fizemos um Wireframe de Alta Fidelidade para a SA. O wireframe foi feito no Figma. ",
+      mais: "Clique para ver mais sobre a atividade",
+      imgUrl: sa,
+      trimestre: "primeiro",
+      link: "https://docs.google.com/document/d/1N2i-uzx4hYPsXLOOdPYMeHanwy9UN7gOrYU0-ZTiuRc/edit?usp=sharing",
+    },
+    {
+      titulo: "Regras de Negócio",
+      descricao:
+        "Nessa atividade fizemos um documento de Regras de Negócio para a SA. O documento foi feito no Google Planilhas. ",
+      mais: "Clique para ver mais sobre a atividade",
+      imgUrl: regras,
+      trimestre: "primeiro",
+      link: "https://docs.google.com/document/d/1gniDpJ0b2ax_Tg4h9W8UJxGyENCdBr3stuTlN0i-Jq8/edit?usp=sharing",
+    },
+    {
+      titulo: "Metodologias de Desenvolvimento",
+      descricao:
+        "Nessa atividade nós fizemos uma sala de aula invertida sobre Metodologias de Desenvolvimento. Nosso grupo ficou responsável por falar sobre a Kanban. ",
+      mais: "Clique para ver mais sobre a atividade",
+      imgUrl: kanban,
+      trimestre: "segundo",
+      link: "https://docs.google.com/document/d/1s5LtI3lKPssqYusBlYKqM7a_2-fiHmsiojZPwy9sy7Y/edit?usp=sharing",
+    },
+  ];
+
+  const dados = [
+    {
+      titulo: "Modelo Lógico",
+      descricao:
+        "Nessa atividade fizemos uma lista de exercícios sobre Modelo Lógico. ",
+      mais: "Clique para ver mais sobre a atividade",
+      imgUrl: logico,
+      trimestre: "segundo",
+      link: "https://docs.google.com/document/d/1O3WwylraSk6qFRZmluvgDSv3MNvD6E_hhAj7EDA1jFk/edit?usp=sharing",
+    },
+    {
+      titulo: "Sistema Gestão de Consultório",
+      descricao:
+        "Nessa atividade fizemos, em grupos, a modelagem de um Sistema de Gestão de Consultório Médico. ",
+      mais: "Clique para ver mais sobre a atividade",
+      imgUrl: medico,
+      trimestre: "segundo",
+      link: "https://docs.google.com/document/d/1_xw_R3wRNFDWlrlLcQDz8A_EIipWBREVDehX4gOHUHQ/edit?usp=sharing",
+    },
+    {
+      titulo: "Normalização de Dados",
+      descricao:
+        "Nessa atividade fizemos uma pesquisa sobre Normalização de Dados, em duplas. ",
+      mais: "Clique para ver mais sobre a atividade",
+      imgUrl: normalização,
+      trimestre: "segundo",
+      link: "https://docs.google.com/document/d/1hPZuR3fVPiPjw_oi6a5PW1ElQZbwLq8HluYiD3fRpaA/edit?usp=sharing",
     },
   ];
 
@@ -93,21 +196,19 @@ export const Tecnico = () => {
                 id="pills-tab"
               >
                 <Nav.Item>
-                  <Nav.Link eventKey="desenvolvimento">
-                    Desenvolvimento De Sistemas
-                  </Nav.Link>
+                  <Nav.Link eventKey="desenvolvimento">Desenvolvimento De Sistemas</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link eventKey="implantação">
-                    Implantação de Software
-                  </Nav.Link>
+                  <Nav.Link eventKey="implantação">Implantação de Software</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link eventKey="modelagem">
-                    Modelagem de Sistemas
-                  </Nav.Link>
+                  <Nav.Link eventKey="modelagem">Modelagem de Sistemas</Nav.Link>
                 </Nav.Item>
-              </Nav>
+                <Nav.Item>
+                  <Nav.Link eventKey="dados">Banco de Dados</Nav.Link>
+                </Nav.Item>
+              </Nav> 
+              <br />           
               <Tab.Content className="tecnico-content">
                 <Tab.Pane eventKey="desenvolvimento">
                   <div className="tecnico-wrapper">
@@ -142,6 +243,20 @@ export const Tecnico = () => {
                   <Row>
                     {selectedCategory === "modelagem" &&
                       modelagem
+                        .filter(
+                          (projeto) => projeto.trimestre === selectedTrimestre
+                        )
+                        .map((projeto, index) => (
+                          <TecnicoCards key={index} {...projeto} />
+                        ))}
+                  </Row>
+                  </div>
+                </Tab.Pane>
+                <Tab.Pane eventKey="dados">
+                <div className="tecnico-wrapper">
+                  <Row>
+                    {selectedCategory === "dados" &&
+                      dados
                         .filter(
                           (projeto) => projeto.trimestre === selectedTrimestre
                         )
